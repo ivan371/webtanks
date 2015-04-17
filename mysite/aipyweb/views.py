06ac76a12040f9be2e852c66d96f638c4452b56d
Ivan <nagaiko.ivan@gmail.com>
2015-04-17 17:55:25 +0300

    web

--- mysite/aipyweb/views.py
+++ mysite/aipyweb/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from field import field
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    newfield = field()
+    file = open('aipyweb/index.html', 'r')
+    return HttpResponse(file)
