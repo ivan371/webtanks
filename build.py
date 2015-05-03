74b36ec6de719c804ea8eaf9af506bd6839bd287
lukanin-artem <lukanin@phystech.edu>
2015-05-03 23:03:05 +0300

    Update build.py

--- build.py
+++ build.py
@@ -2,8 +2,8 @@ import os
 
 os.system("git clone https://github.com/ivan371/webtanks")
 
-os.chdir("webtanks/sources/mysite")
+os.chdir("webtanks/sources/tanks")
 
 os.system("python manage.py runserver")
 
-os.system("gnome-open http://127.0.0.1:8000/aipyweb")
+os.system("gnome-open http://127.0.0.1:8000/webtanks")
