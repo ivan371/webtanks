127363a60b5d6be2bb422d947d431c69860f5ba0
Ivan <nagaiko.ivan@gmail.com>
2015-05-31 14:44:52 +0300

    correct two tanks

--- sources/webtanks/static/multitanks/change.js
+++ sources/webtanks/static/multitanks/change.js
@@ -1,34 +1,33 @@
 function change(type, X, Y)
 	{
-		
-		switch (type){	//1 - up; //2 - down //3 - right // 4 - left
-		case 1:
-			img.src = t1;
-			break;
-		case 2:
-			img.src = t2;
-			break;
-		case 3:
-			img.src = t3;
-			break;
-		case 4:
-			img.src = t4;
-			break;
-		case 5:
-			img.src = t5;
-			break;
-		case 6:
-			img.src = t6;
-			break;
-		case 7:
-			img.src = t7;
-			break;
-		case 8:
-			img.src = t8;
-			break;
-		}
 		if(who == 1)
 		{
+			switch (type){	//1 - up; //2 - down //3 - right // 4 - left
+			case 1:
+				img.src = t1;
+				break;
+			case 2:
+				img.src = t2;
+				break;
+			case 3:
+				img.src = t3;
+				break;
+			case 4:
+				img.src = t4;
+				break;
+			case 5:
+				img.src = t5;
+				break;
+			case 6:
+				img.src = t6;
+				break;
+			case 7:
+				img.src = t7;
+				break;
+			case 8:
+				img.src = t8;
+				break;
+			}
 			ctx.clearRect(X-10, Y-10, 40, 40);
       		img.onload = function(){		
 				ctx.drawImage(img, 100, 100, 800, 800, X, Y, 20, 20);
@@ -36,6 +35,32 @@ function change(type, X, Y)
 		}
 		else
 		{
+			switch (type){	//1 - up; //2 - down //3 - right // 4 - left
+			case 1:
+				img.src = b1;
+				break;
+			case 2:
+				img.src = b2;
+				break;
+			case 3:
+				img.src = b3;
+				break;
+			case 4:
+				img.src = b4;
+				break;
+			case 5:
+				img.src = b5;
+				break;
+			case 6:
+				img.src = b6;
+				break;
+			case 7:
+				img.src = b7;
+				break;
+			case 8:
+				img.src = b8;
+				break;
+			}
 			otx.clearRect(X-10, Y-10, 40, 40);
 			img.onload = function(){	
 				otx.drawImage(img, 100, 100, 800, 800, X, Y, 20, 20);
